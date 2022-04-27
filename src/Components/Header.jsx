8c7b6474cfcb7d6804b07aec59a498292e8fa942
LucassNogueira <simplylucasjason@gmail.com>
2022-04-27 18:22:59 -0500

    added little margin on group svg

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,8 @@ import { ReactComponent as Group } from "../icons/group.svg";
 const Header = () => {
   return (
     <div className="flex justify-center items-center h-[64px] shadow-lg bg-white">
-      <Group />
-      <h1 className="uppercase text-lg text-[#4A4A4A]">Users</h1>
+      <Group className="mr-1" />{" "}
+      <h1 className="uppercase text-lg text-[#4A4A4A]"> Users</h1>
     </div>
   );
 };
