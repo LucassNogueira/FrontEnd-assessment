712a95121b7a2e44522bbf75d578c57545cc8a49
LucassNogueira <simplylucasjason@gmail.com>
2022-04-27 16:27:17 -0500

    got the dependent showing of details now just gotta manipulate some data

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 import { ReactComponent as UserIcon } from "../icons/user.svg";
 import { ReactComponent as More } from "../icons/expand_more.svg";
 import { ReactComponent as Less } from "../icons/expand_less.svg";
+
 const UserCard = ({ user, index }) => {
   const [showing, setShowing] = useState(null);
-
+  const [open, setOpen] = useState(false);
   function toggle(index) {
     if (showing == index) {
+      setOpen(false);
       return setShowing(null);
     }
     setShowing(index);
+    setOpen(true);
   }
-  function findFill(role) {
+  function roleFill(role) {
     if (role === "Administrator") {
       return "#2081C3";
     } else if (role === "User") {
@@ -23,41 +26,48 @@ const UserCard = ({ user, index }) => {
     }
   }
   return (
-    <div className="relative flex shadow-lg  bg-white mt-4 mx-3 h-20 justify-start  ">
-      <UserIcon
-        width={50}
-        height={50}
-        className="mt-4"
-        fill={findFill(user.role)}
-      />
+    <div
+      onClick={() => toggle(index)}
+      className={`relative flex shadow-lg  bg-white  mt-4 mx-3 pb-4 pt-4`}
+    >
+      <UserIcon width={50} height={50} fill={roleFill(user.role)} />
 
-      <div className="mt-4 ml-3">
+      <div className="flex flex-col just ml-3">
         <h1 className="font-semibold text-[12px] text-[#4A4A4A]">
           {user.firstName + " " + user.lastName}
         </h1>
         <h2 className="text-[10px] text-[#4A4A4A]">{user.role}</h2>
-        <h3 className="text-[10px]  ">
+        <p className="text-[10px]  ">
           <a
             className=" text-[#7E7E7E] hover:text-blue-700 visited:text-[#7E7E7E]"
             href={`mailto:${user.email}`}
           >
             {user.email}
           </a>
-        </h3>
+        </p>
+        {open ? (
+          <div className="h-[190px] w-[200px] ">
+            <h3 className="text-[10px] font-semibold text-[#4A4A4A] mt-4">
+              Address
+            </h3>
+            <p className="text-[10px] text-[#4A4A4A]">{`${user.street}, ${user.city}, ${user.state} ${user.zip}`}</p>
+            <h3 className="text-[10px] font-semibold text-[#4A4A4A] mt-4">
+              Phone
+            </h3>
+            <h3 className="text-[10px] font-semibold text-[#4A4A4A] mt-4">
+              Created At
+            </h3>
+            <h3 className="text-[10px] font-semibold text-[#4A4A4A] mt-4">
+              Last Logged In
+            </h3>
+          </div>
+        ) : null}
       </div>
 
       {showing !== user.id ? (
-        <More
-          fill="#7E7E7E"
-          onClick={() => toggle(index)}
-          className="mt-7 absolute right-5"
-        />
+        <More fill="#7E7E7E" className=" absolute right-5 top-7" />
       ) : (
-        <Less
-          fill="#7E7E7E"
-          className="mt-7 absolute right-5"
-          onClick={() => toggle(index)}
-        />
+        <Less fill="#7E7E7E" className="absolute right-5 top-7" />
       )}
     </div>
   );
