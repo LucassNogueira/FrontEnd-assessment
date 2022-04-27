6e6b6d1df80c8ebb83cefb52351f9fffdc65a9b2
LucassNogueira <simplylucasjason@gmail.com>
2022-04-27 12:33:00 -0500

    getting SVG added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import usersSvg from "../icons/user.svg";
+const Header = () => {
+  return (
+    <div className="uppercase text-lg text-[#4A4A4A]">
+      <usersSvg />
+      Users
+    </div>
+  );
+};
+
+export default Header;
